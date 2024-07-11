b945a13c36f369df9c9bfc88654272c21f672d0a
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-07-11 01:12:36 +0100

    Initial commmit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("node:path");
 const cors = require("cors");
 const { router } = require("./src/routers");
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+
 server.use("/api", router);
+server.use("/", express.static(path.join(__dirname, "src", "view")));
 
 const { PORT = 5173 } = process.env;
 
